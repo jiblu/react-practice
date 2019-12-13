0374e18dd09992ea47daf266cc4854f692cf7c2c
ji <jkim0107@gmail.com>
2019-12-13 13:38:44 -0800

    setState practice

--- theCompleteGuide/react-complete-guide/src/App.js
+++ theCompleteGuide/react-complete-guide/src/App.js
@@ -11,13 +11,24 @@ class App extends Component {
     ]
   };
 
+  switchNameHandler = () => {
+    console.log('Was clicked')
+    this.setState({
+      persons: [
+        { name: 'Jihyun', age: 31 },
+        { name: 'Junghyun', age: 28},
+        { name: 'Munjidog', age: 2}
+      ]
+    })
+  }
+
   render() {
     const { persons } = this.state;
 
     return (
       <div className="App">
         <h1>Hi, Im a react App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={persons[0].name} age={persons[0].age}>I like cakes</Person>
         <Person name={persons[1].name} age={persons[1].age}>I like chocolate</Person>
         <Person name={persons[2].name} age={persons[2].age}>I like pie</Person>
