e77f4c4c8045b0d21ea67e0ffc4558e9dca0d0bf
ji <jkim0107@gmail.com>
2019-12-13 15:57:39 -0800

    preferred method of conditional rendering

--- theCompleteGuide/react-complete-guide/src/App.js
+++ theCompleteGuide/react-complete-guide/src/App.js
@@ -39,24 +39,29 @@ class App extends Component {
     };
 
     const { persons } = this.state;
+
+    let people = null;
+    if (this.state.showPersons) {
+      people = (
+        <div>
+        <Person name={persons[0].name} age={persons[0].age}>I like cakes</Person>
+        <Person 
+          name={persons[1].name} 
+          age={persons[1].age}
+          click={this.switchNameHandler.bind(this, 'bound!')}>I like chocolate</Person>
+        <Person name={persons[2].name} age={persons[2].age}>I like pie</Person>
+        <Person name={"Jung"}></Person>
+      </div>
+      )
+    }
+
     return (
       <div className="App">
           <h1>Hi, Im a react App</h1>
           <button 
             style={style}
-            onClick={this.togglePersonHandler}>Switch Name</button>
-        
-        { this.state.showPersons ? 
-          <div>
-            <Person name={persons[0].name} age={persons[0].age}>I like cakes</Person>
-            <Person 
-              name={persons[1].name} 
-              age={persons[1].age}
-              click={this.switchNameHandler.bind(this, 'bound!')}>I like chocolate</Person>
-            <Person name={persons[2].name} age={persons[2].age}>I like pie</Person>
-            <Person name={"Jung"}></Person>
-          </div> : null
-        }  
+            onClick={this.togglePersonHandler}>Toggle showPersons</button>
+          {people}
       </div>
       );
   }
