bf1b2cb595b2b61fbdc861b5b0e414063ae2474c
ji <jkim0107@gmail.com>
2019-12-13 13:48:43 -0800

    reactHooks implementation

--- theCompleteGuide/react-complete-guide/src/App.js
+++ theCompleteGuide/react-complete-guide/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState] = useState({
     persons: [
-      { name: 'Ji', age: 31 },
-      { name: 'Jung', age: 28},
-      { name: 'Munji', age: 2}
-    ]
-  };
+        { name: 'Ji', age: 31 },
+        { name: 'Jung', age: 28},
+        { name: 'Munji', age: 2}
+      ]
+  })
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     console.log('Was clicked')
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Jihyun', age: 31 },
         { name: 'Junghyun', age: 28},
@@ -21,21 +21,18 @@ class App extends Component {
       ]
     })
   }
-
-  render() {
-    const { persons } = this.state;
-
-    return (
-      <div className="App">
+  const { persons } = personsState;
+  
+  return (
+    <div className="App">
         <h1>Hi, Im a react App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={switchNameHandler}>Switch Name</button>
         <Person name={persons[0].name} age={persons[0].age}>I like cakes</Person>
         <Person name={persons[1].name} age={persons[1].age}>I like chocolate</Person>
         <Person name={persons[2].name} age={persons[2].age}>I like pie</Person>
         <Person name={"Jung"}></Person>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
