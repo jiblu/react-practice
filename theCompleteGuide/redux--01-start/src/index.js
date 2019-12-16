d396f6d99921bf0a7ce2434adb90c7f286288b2b
ji <jkim0107@gmail.com>
2019-12-15 16:19:18 -0800

    added reducer to main index

--- theCompleteGuide/redux--01-start/src/index.js
+++ theCompleteGuide/redux--01-start/src/index.js
@@ -6,6 +6,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const initialState = {
+    counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
 const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
