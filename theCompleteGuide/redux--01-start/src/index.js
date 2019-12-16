deca685fd67287d042fe7bf9a879466541b3c2f4
ji <jkim0107@gmail.com>
2019-12-15 16:20:37 -0800

    moved reducer to own file in store

--- theCompleteGuide/redux--01-start/src/index.js
+++ theCompleteGuide/redux--01-start/src/index.js
@@ -5,14 +5,8 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-const initialState = {
-    counter: 0
-};
-
-const reducer = (state = initialState, action) => {
-    return state;
-};
 
 const store = createStore(reducer);
 
